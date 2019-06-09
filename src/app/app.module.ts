83f1d77b251f268750a975d912d7ef6b83a09fc5
Matt Scheetz <mfscheetz@gmail.com>
2019-06-08 23:13:48 -0400

    trying to avoid cors issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { MatTableModule } from '@angular/material'  
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/binance-svc.service.ts
+++ src/app/services/binance-svc.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Ticker } from '../classes/Binance/Ticker';
 import { Kline } from '../classes/Binance/Kline';
 import { ExchangeInfo } from '../classes/Binance/ExchangeInfo';
@@ -10,19 +10,33 @@ export class BinanceService {
     constructor(private http: HttpClient) {}
 
     base: string = "https://api.binance.com";
+    httpOptions = {
+        headers: new HttpHeaders({
+            "Access-Control-Allow-Methods": "GET, POST, DELETE, PUT",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, content-type"
+        })
+    };
+    // httpOptions = {
+    // headers: new HttpHeaders().
+    // set('Content-Type', 'application/json').
+    // set('Accept', 'application/json').
+    // set('Access-Control-Allow-Headers', 'Content-Type').
+    // set('Access-Control-Allow-Origin', '*')
+    // };
 
     getExchangeInfo(): Observable<ExchangeInfo> {
         let endpoint: string = "/api/v1/exchangeInfo";
         let url: string = this.base + endpoint;
 
-        return this.http.get<ExchangeInfo>(url);
+        return this.http.get<ExchangeInfo>(url, this.httpOptions);
     }
 
     getTickers(): Observable<Ticker[]> {
         let endpoint: string = "/api/v1/ticker/24hr";
         let url: string = this.base + endpoint;
 
-        return this.http.get<Ticker[]>(url);
+        return this.http.get<Ticker[]>(url, this.httpOptions);
     }
 
     getKline(pair: string, interval: string): Observable<Kline[]> {
@@ -30,6 +44,6 @@ export class BinanceService {
         let qs: string = "?symbol="+ pair +"&interval="+interval;
         let url: string = this.base + endpoint + qs;
 
-        return this.http.get<Kline[]>(url);
+        return this.http.get<Kline[]>(url, this.httpOptions);
     }
 }
